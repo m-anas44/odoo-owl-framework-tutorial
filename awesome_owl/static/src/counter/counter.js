ba1c8b536c9d769e642d151c271a6fbd3a79d17d
Muhammad Anas <manasansari1438@gmail.com>
2025-08-29 17:26:50 +0500

    [ Using markup to display html ] done

---
+++ awesome_owl/static/src/counter/counter.js
@@ -0,0 +1,13 @@
+import { Component, xml } from "@odoo/owl";
+
+export class Counter extends Component {
+  static template = xml`
+        <p class="d-flex gap-2 align-items-center border p-2 m-0">
+                <span class="p-1">
+                    Counter:
+                    <t t-esc="props.value"/>
+                </span>
+            <button class="btn btn-primary bg-secondary text-white" t-on-click="props.t-on-click">Increment</button>
+        </p>
+    `;
+}
--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,11 +1,16 @@
 /** @odoo-module **/
 
-import { Component, useState } from "@odoo/owl";
+import { Component, useState, markup } from "@odoo/owl";
+import { Counter } from "./counter/counter";
+import { Card } from "./card/card";
 
 export class Playground extends Component {
   static template = "awesome_owl.playground";
+  static components = { Counter, Card };
   setup() {
-    this.state = useState({ value: 0 });
+    this.state = useState({ value: 1 });
+    this.normalContent = "<b>This will not be bold</b>";
+    this.markupContent = markup("<b>This WILL be bold</b>")
   }
   increment() {
     this.state.value++;
