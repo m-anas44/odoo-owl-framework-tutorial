ed5c19fefcf9183ba26bafb7777111bfe803d49c
Muhammad Anas <manasansari1438@gmail.com>
2025-09-04 22:55:02 +0500

    card with slots & minimizing card content

--- awesome_owl/static/src/card/card.js
+++ awesome_owl/static/src/card/card.js
@@ -1,19 +1,29 @@
-import { Component, xml } from "@odoo/owl";
+import { Component, xml, useState } from "@odoo/owl";
 export class Card extends Component {
   static template = xml`
     <div class="card d-inline-block m-2" style="width: 18rem;">
         <div class="card-body">
-            <h5 class="card-title">
-                <t t-esc="props.title" />
+            <h5 class="card-title d-flex justify-content-between align-items-center">
+              <t t-esc="props.title" />
+              <button class="btn btn-sm btn-outline-primary"
+                      t-on-click="toggleOpen">
+                <t t-esc="state.open ? 'Hide' : 'Show'"/>
+              </button>
             </h5>
-            <p class="card-text">
-                <t t-out="props.content" />
-            </p>
+            <div t-if="state.open" class="card-text">
+              <t t-slot="default"/>
+            </div>
         </div>
     </div>
     `;
   static props = {
     title: { type: String },
-    content: { type: String },
+    slots: { type: Object, optional: true },
   };
+  setup() {
+    this.state = useState({ open: true });
+  }
+  toggleOpen() {
+    this.state.open = !this.state.open;
+  }
 }
