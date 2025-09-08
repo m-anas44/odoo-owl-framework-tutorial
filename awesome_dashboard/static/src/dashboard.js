f88ff64a74560baee3a3b66e471b9f98e8be0b9c
Muhammad Anas <manasansari1438@gmail.com>
2025-09-08 12:51:45 +0500

    Call the server, add some statistics

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -1,10 +1,43 @@
 /** @odoo-module **/
-
-import { Component } from "@odoo/owl";
+import { Component, useState, onWillStart } from "@odoo/owl";
 import { registry } from "@web/core/registry";
+import { Layout } from "@web/search/layout";
+import { _t } from "@web/core/l10n/translation";
+import { useService } from "@web/core/utils/hooks";
+import { DashboardItem } from "./DashboardItem/DashboardItem";
+import { rpc } from "@web/core/network/rpc";
 
 class AwesomeDashboard extends Component {
-    static template = "awesome_dashboard.AwesomeDashboard";
+  static template = "awesome_dashboard.AwesomeDashboard";
+  static components = { Layout, DashboardItem };
+  setup() {
+    this.display = { controlPanel: {} };
+    this.action = useService("action");
+    this.stats = useState({});
+    onWillStart(async () => {
+      this.stats = await rpc("/awesome_dashboard/statistics", {});
+      console.log(this.stats);
+    });
+  }
+
+  async getCustomers() {
+    this.action.doAction("base.action_partner_form");
+  }
+
+  async getCrmLeads() {
+    this.action.doAction({
+      type: "ir.actions.act_window",
+      name: _t("Leads"),
+      target: "current",
+      res_model: "crm.lead",
+      views: [
+        [false, "list"],
+        [false, "form"],
+      ],
+    });
+  }
 }
 
-registry.category("actions").add("awesome_dashboard.dashboard", AwesomeDashboard);
+registry
+  .category("actions")
+  .add("awesome_dashboard.dashboard", AwesomeDashboard);
