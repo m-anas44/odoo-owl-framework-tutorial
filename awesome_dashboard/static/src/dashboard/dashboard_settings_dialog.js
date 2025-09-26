6275170a772cf212e4c8874b43cca024e43981c3
Muhammad Anas <manasansari1438@gmail.com>
2025-09-26 23:20:39 +0500

    notification service added

--- awesome_dashboard/static/src/dashboard/dashboard_settings_dialog.js
+++ awesome_dashboard/static/src/dashboard/dashboard_settings_dialog.js
@@ -1,5 +1,7 @@
 import { Component, useState } from "@odoo/owl";
 import { Dialog } from "@web/core/dialog/dialog";
+import { useService } from "@web/core/utils/hooks";
+import { _t } from "@web/core/l10n/translation";
 
 export class DashboardSettingsDialog extends Component {
   static template = "awesome_dashboard.DashboardSettingsDialog";
@@ -7,6 +9,7 @@ export class DashboardSettingsDialog extends Component {
     this.state = useState({
       selected: new Set(this.props.filteredItems.map((item) => item.id)),
     });
+    this.notification = useService("notification");
   }
 
   toggle(id) {
@@ -18,10 +21,19 @@ export class DashboardSettingsDialog extends Component {
   }
 
   apply() {
-    const allIds = this.props.items.map((i) => i.id);
-    const removed = allIds.filter((id) => !this.state.selected.has(id));
-    localStorage.setItem("dashboard_removed", JSON.stringify(removed));
-    location.reload(); // simplest way: reload dashboard to reapply
+    try {
+      const allIds = this.props.items.map((i) => i.id);
+      const removed = allIds.filter((id) => !this.state.selected.has(id));
+      localStorage.setItem("dashboard_removed", JSON.stringify(removed));
+      this.notification.add(_t("Dashboard Setting Updated!"), {
+        type: "success",
+      });
+      location.reload(); // simplest way: reload dashboard to reapply
+    } catch (error) {
+      this.notification.add(_t("Couldn't make changes."), {
+        type: "danger",
+      });
+    }
   }
 
   static components = { Dialog };
